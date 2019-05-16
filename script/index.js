9501e4f5fdefe7d59baaf09d83748a2b794588c4
rajat-29 <rajat295gupta@gmail.com>
2019-05-16 13:49:27 +0530

    interface changes

---
+++ script/index.js
@@ -0,0 +1,33 @@
+var x = document.getElementById('abt');
+
+var s=['Software Engineer','FullStack Developer','Coder','Android Developer','Student'];
+
+var i = 0;
+
+function display(i){
+    if(i >= s.length){
+        i = 0;
+    }
+
+    x.innerHTML = s[i];
+    setTimeout(function(){
+       display(i + 1)
+    }, 3000)
+}
+
+display(0);
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
